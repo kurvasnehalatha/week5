bd6cce1e6d155181a8f2f828317751f7db8501fe
kurvasnehalatha <kurvasnehalatha06@gmail.com>
2026-08-12 10:31:13 +0530

    Correct factorial loop initialization and syntax
    Fix loop initialization and syntax error in factorial calculation.

--- Factorial.java
+++ Factorial.java
@@ -5,8 +5,8 @@ public class Factorial {
         }
 
         int result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
+        for (int i = ; i <= n; i++) {
+            result *= i
         }
         return result;
     }
