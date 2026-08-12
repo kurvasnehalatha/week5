996156176e3f99ad681c6ab51af28302566ab05d
kurvasnehalatha <kurvasnehalatha06@gmail.com>
2026-08-12 10:23:49 +0530

    Factorial Prog

---
+++ TestFactorial.java
@@ -0,0 +1,8 @@
+public class TestFactorial {
+    public static void main(String[] args) {
+        int number = 5;
+        int result = Factorial.factorial(number);
+
+        System.out.println("Factorial of " + number + " is " + result);
+    }
+}
